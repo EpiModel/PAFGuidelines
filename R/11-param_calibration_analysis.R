c6702ad11a3fecd43638a377c214d1deb5ded4ff
Adrien Le Guillou <adrien.leguillou@gmail.com>
2021-05-18 09:29:37 +0200

    update EMHIV

--- R/11-param_calibration_analysis.R
+++ R/11-param_calibration_analysis.R
@@ -1,15 +1,39 @@
 library(data.table)
+library(purrr)
+library(dplyr)
+library(tidyr)
 
 # One or many job_names
-job_names <- c("CPN_new_EMH")
+job_names <- "PAF_sti_baseline"
 job_last_n <- NULL # if not NULL, get last N jobs. Otherwise, use job_names
-keep_all_epi <- TRUE # set to false to keep only the "targets"
 
 if (!is.null(job_last_n))
   job_names <- tail(readLines("out/remote_jobs/last_jobs"), job_last_n)
 
 # Read targets
 source("R/utils-targets.R")
+
+needed_trackers <- c(
+  "n",
+  "i", "i_dx", "i_sup", "linked1m",
+  "s", "s_prep", "s_prep_elig",
+  "gc_s", "ct_s"
+)
+
+needed_pops <- c("B", "H", "W")
+
+needed_trackers <- vapply(
+  needed_pops,
+  function(pop) paste0(needed_trackers, "___", pop),
+  needed_trackers
+)
+
+needed_cols <- c(
+ "sim", "time", "batch", "param_batch",
+  "incid.gc.B", "incid.gc.H", "incid.gc.W",
+  "incid.ct.B", "incid.ct.H", "incid.ct.W",
+  needed_trackers
+)
 
 jobs <- list()
 for (job in job_names) {
@@ -24,63 +48,13 @@ for (job in job_names) {
     btch <- as.numeric(stringr::str_extract(fs::path_file(fle), "\\d+"))
     sim <- readRDS(fle)
     dff <- as.data.table(sim)
-    dff[, `:=`(batch = btch, param_batch = infos$unique_proposals[btch])]
-    # Keep only the "targets"
-    if (! keep_all_epi) {
-      dff <- dff[,
-        .SD, .SDcols = c("param_batch", "batch", "sim", "time", names(targets))]
-    }
-    # do some transforms here (or not but risk memory overflow)
-    #
-    df_ls[[btch]] <- dff
+    dff[, `:=`(batch = btch)]
+    keep_cols <- intersect(needed_cols, names(dff))
+    df_ls[[btch]] <- dff[, ..keep_cols]
   }
-  jobs[[job]]$data <- rbindlist(df_ls)
+  df_ls <- discard(df_ls, ~ is.null(.x))
+  jobs[[job]]$data <- rbindlist(df_ls, fill = TRUE)
 }
 
-library(tidyverse)
-
-df <- as_tibble(jobs[[1]]$data)
-proposals <- jobs[[1]]$infos$param_proposals[1:max(df$param_batch)]
-
-# df <- filter(df, time >= max(time) - 52)
-
-source("R/utils-targets.R")
-prevs <- targets[startsWith(names(targets), "i.prev")]
-
-m <- 0.003
-lst <- df %>%
-  group_by(param_batch) %>%
-  summarise(across(c(i.prev.dx.B, i.prev.dx.H, i.prev.dx.W), median)) %>%
-  arrange(i.prev.dx.B) %>%
-  filter(
-    between(i.prev.dx.B, prevs["i.prev.dx.B"] - m, prevs["i.prev.dx.B"] + m),
-    between(i.prev.dx.H, prevs["i.prev.dx.H"] - m, prevs["i.prev.dx.H"] + m),
-    between(i.prev.dx.W, prevs["i.prev.dx.W"] - m, prevs["i.prev.dx.W"] + m)
-  ) %>%
-  print(n = 200) %>%
-  pull(param_batch)
-
-proposals[lst]
-
-d2 <- df %>%
-  group_by(param_batch) %>%
-  summarise(across(
-    c(i.prev.dx.B, i.prev.dx.H, i.prev.dx.W),
-    list(
-      q1 = ~ quantile(.x, prob = 0.25),
-      q2 = ~ quantile(.x, prob = 0.5),
-      q3 = ~ quantile(.x, prob = 0.75)
-    )
-  ))
-
-
-df %>%
-  group_by(param_batch) %>%
-  summarise(across(
-    c(ir100.gc, ir100.ct),
-    list(
-      # q1 = ~ quantile(.x, prob = 0.25),
-      q2 = ~ quantile(.x, prob = 0.5)
-      # q3 = ~ quantile(.x, prob = 0.75)
-    )
-  ))
+df <- map_dfr(jobs, ~ as_tibble(.x$data))
+saveRDS(df, "out/scdf.rds")
